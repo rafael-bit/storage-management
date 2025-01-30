d5696e29c7457561fb88a7d999f2a7fd2352474c
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-01-30 02:30:55 -0300

    feat: create sort module

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client";
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 export default function Sort() {
+	const path = usePathname();
+	const router = useRouter();
+
+	const handleSort = (value: string) => {
+		router.push(`${path}?sort=${value}`);
+	};
+
 	return (
-		<div>Sort</div>
+		<Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+			<SelectTrigger className="outline-none ring-offset-transparent focus:ring-transparent focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 h-11 w-full rounded-[8px] border-transparent bg-white !shadow-sm sm:w-[210px]">
+				<SelectValue placeholder={sortTypes[0].value} />
+			</SelectTrigger>
+			<SelectContent>
+				{sortTypes.map((sort) => (
+					<SelectItem
+						key={sort.label}
+						value={sort.value}
+					>
+						{sort.label}
+					</SelectItem>
+				))}
+			</SelectContent>
+		</Select>
 	)
 }
